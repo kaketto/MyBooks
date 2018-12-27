c5982e95f78a8dd06310fe6709f7f2973a3a5364
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-12-27 22:40:50 +0100

    fix: remove repetition of logic

--- src/Bookshelves.js
+++ src/Bookshelves.js
@@ -3,39 +3,24 @@ import PropTypes from 'prop-types'
 import Book from './Book'
 import { Link } from 'react-router-dom'
 
+const shelves = ['currentlyReading', 'wantToRead', 'read'];
+const shelfNames = ['Currently Reading', 'Want to read', 'Read']
+
 const Bookshelves = ({ books, onUpdateShelf }) => (
   <div>
     <h1 className="header">My Books</h1>
-    <div className="bookshelf">
-      <h2>Currently Reading</h2>
-      <ul className="book-list">
-        {books.filter(book => book.shelf === 'currentlyReading').map(book => (
+    {shelves.map((shelf, index) => (
+      <div className="bookshelf">
+        <h2>{shelfNames[index]}</h2>
+        <ul className="book-list">
+        {books.filter(book => book.shelf === shelf).map(book => (
           <li key={book.id}>
             <Book book={book} onUpdateShelf={onUpdateShelf} />
           </li>
         ))}
-      </ul>
-    </div>  
-    <div className="bookshelf">
-      <h2>Want to read</h2>
-      <ul className="book-list">
-        {books.filter(book => book.shelf === 'wantToRead').map(book => (
-          <li key={book.id}>
-            <Book book={book} onUpdateShelf={onUpdateShelf} />
-          </li>
-        ))}
-      </ul>
-    </div>  
-    <div className="bookshelf">
-      <h2>Read</h2>
-      <ul className="book-list">
-        {books.filter(book => book.shelf === 'read').map(book => (
-          <li key={book.id}>
-            <Book book={book} onUpdateShelf={onUpdateShelf} />
-          </li>
-        ))}
-      </ul>
-    </div>
+        </ul>
+      </div>  
+    ))}
     <Link
       to="/search"
       className="search-link"
